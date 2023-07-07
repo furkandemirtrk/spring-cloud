7e094590d550c1c5ef7887b00c53833fb1a5fffd
Furkan Demirtürk <furkan-demirtrk@yandex.com>
2023-07-08 01:13:49 +0300

    spring boot admin service created

---
+++ adminservice/src/test/java/com/demirturk/adminservice/AdminserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demirturk.adminservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
