e3d31bd078062d6608406942dd1816986a73debd
Furkan Demirtürk <furkan-demirtrk@yandex.com>
2023-07-08 16:15:40 +0300

    dockerfile created for services

--- adminservice/src/main/java/com/demirturk/adminservice/AdminserviceApplication.java
+++ adminservice/src/main/java/com/demirturk/adminservice/AdminserviceApplication.java
@@ -2,12 +2,9 @@ package com.demirturk.adminservice;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableAdminServer
 public class AdminserviceApplication {
 
